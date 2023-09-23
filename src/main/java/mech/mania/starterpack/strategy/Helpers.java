e5188dbdc7744af35425fcf5bb7ecabf84754243
Xinyang Li <112905845+XinyangLi1@users.noreply.github.com>
2023-09-23 03:10:24 -0500

    Fixed helpers.java

--- src/main/java/mech/mania/starterpack/strategy/Helpers.java
+++ src/main/java/mech/mania/starterpack/strategy/Helpers.java
@@ -1,7 +1,9 @@
 package mech.mania.starterpack.strategy;
 import mech.mania.starterpack.game.character.Character;
+import mech.mania.starterpack.game.character.action.AbilityActionType;
 import mech.mania.starterpack.game.util.Position;
 import java.lang.Math;
+import mech.mania.starterpack.game.character.action.AbilityAction;
 public class Helpers{
     public static int ManhattonDistanceFunction(
         Character _currentPerson,
@@ -15,7 +17,6 @@ public class Helpers{
     ) {
         return Math.max(Math.abs(_currentPerson.position().x() - _destination.x()), Math.abs(_currentPerson.position().y()-_destination.y()));
     }
-}
     public static AbilityAction Build(
         String id,
         Position _buildPosition
